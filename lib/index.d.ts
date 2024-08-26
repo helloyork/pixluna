61f90c7358e2c74e95c908b9dd495dd99bc8a417
星野梦月 <147403913+Hoshino-Yumetsuki@users.noreply.github.com>
2024-08-26 11:52:46 +0800

    Delete lib directory

--- lib/index.d.ts
+++
@@ -1,5 +0,0 @@
-import { Context } from 'koishi';
-export declare const name = "@q78kg/pixiv";
-import Config from "./config";
-export * from './config';
-export declare function apply(ctx: Context, config: Config): void;
