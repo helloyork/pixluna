048f4b3d8d9e308e97bd29c522b64de65a2f1eae
helloyork <helloyork@icloud.com>
2024-08-27 23:11:47 -0700

    增加图片扰乱

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,8 @@
 import { Context, Random } from 'koishi';
 import { HttpUtil } from "./util/HttpUtil";
 import { AxiosRequestConfig, Method } from "axios";
+import Jimp from 'jimp';
+import JimpConstructors from 'jimp/types';
 
 export const name = '@q78kg/pixiv';
 
@@ -37,9 +39,34 @@ export function apply(ctx: Context, config: Config) {
               </message>
             );
           } else {
+            const imageBuffer = await readRemoteImage(image.urls.original, (image) => {
+              const dotDiameter = 10;
+
+              const x = image.bitmap.width - dotDiameter;
+              const y = image.bitmap.height - dotDiameter;
+
+              for (let i = 0; i < dotDiameter; i++) {
+                for (let j = 0; j < dotDiameter; j++) {
+                  const color = image.getPixelColor(x + i, y + j);
+                  const _r = (color >> 24) & 0xFF;
+                  const r = _r + 10 > 255 ? _r - 10 : _r + 10;
+                  const _g = (color >> 16) & 0xFF;
+                  const g = _g + 10 > 255 ? _g - 10 : _g + 10;
+                  const _b = (color >> 8) & 0xFF;
+                  const b = _b + 10 > 255 ? _b - 10 : _b + 10;
+                  const a = color & 0xFF;
+                  const newColor = Jimp.rgbaToInt(r, g, b, a);
+                  image.setPixelColor(newColor, x + i, y + j);
+
+                }
+              }
+            });
+
+            const dataurl = arrayBufferToDataUrl(imageBuffer, getImageMimeType(image.urls.original));
+
             messages.push(
               <message>
-                <image url={image.urls.original}></image>
+                <image url={dataurl}></image>
                 <text content={`\ntitle：${image.title}\n`}></text>
                 <text content={`id：${image.pid}\n`}></text>
                 <text content={`tags：${image.tags.map((item) => {
@@ -115,3 +142,36 @@ const getAxiosConfig = (): AxiosRequestConfig | undefined => {
     method: 'GET' as Method,
   };
 };
+
+function getImageMimeType(url: string): string {
+  const MimeType = url.split('.').pop();
+  const TypeMap = {
+    jpg: "image/jpeg",
+    jpeg: "image/jpeg",
+    png: "image/png",
+    gif: "image/gif",
+  };
+  return TypeMap[MimeType];
+}
+
+async function readRemoteImage(url: string, onRead?: (image: JimpConstructors) => void): Promise<ArrayBuffer> {
+  const MimeType = url.split('.').pop();
+  const TypeMap = {
+    jpg: Jimp.MIME_JPEG,
+    jpeg: Jimp.MIME_JPEG,
+    png: Jimp.MIME_PNG,
+    gif: Jimp.MIME_GIF,
+  };
+  return Jimp.read(url).then((image) => {
+    if (onRead) {
+      onRead(image);
+    }
+    return image.getBufferAsync(TypeMap[MimeType]);
+  });
+}
+
+function arrayBufferToDataUrl(arrayBuffer: ArrayBuffer, type: string): string {
+  const blob = new Blob([arrayBuffer], { type });
+  return URL.createObjectURL(blob);
+}
+
