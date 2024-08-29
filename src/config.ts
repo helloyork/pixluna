490c0fb8714c9efc7b3bf9fc5aa551fbdc6e1850
Hoshino-Yumetsuki <hoshino-yumetsuki@outlook.com>
2024-08-29 09:10:30 +0800

    增加可读性

--- src/config.ts
+++ src/config.ts
@@ -14,7 +14,7 @@ export interface Config {
 export const Config: Schema<Config> = Schema.intersect([
   Schema.object({
     // 通用设置
-    isR18: Schema.boolean().default(false).description('是否开启 R18 内容过滤'),
+    isR18: Schema.boolean().default(false).description('是否允许返回 R18 内容'),
     isProxy: Schema.boolean().default(false).description('是否使用代理'),
     excludeAI: Schema.boolean().default(false).description('是否排除 AI 生成作品'),
     baseUrl: Schema.string().default('i.pixiv.re').description('图片反代服务的地址'),
@@ -23,24 +23,18 @@ export const Config: Schema<Config> = Schema.intersect([
 
   Schema.union([
     Schema.object({
-      isProxy: Schema.const(false),
-    }).description('不使用代理'),
-    Schema.object({
       isProxy: Schema.const(true),
       proxyHost: Schema.string().default('http://127.0.0.1:7890').description('代理服务器地址'),
     }).description('代理设置')
-  ]).description('代理配置'),
+  ]),
 
   Schema.union([
     Schema.object({
-      isR18: Schema.const(false),
-    }).description('禁用 R18 内容过滤'),
-    Schema.object({
       isR18: Schema.const(true),
-      r18P: Schema.percent().default(0.1).description('R18 内容的概率')
+      r18P: Schema.percent().default(0.1).description('R18 内容出现的概率')
         .min(0).max(1).step(0.01),
     }).description('R18 内容设置')
-  ]).description('R18 配置')
+  ])
 ]);
 
 export default Config;
